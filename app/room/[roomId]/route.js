661bf1d1c31548ae42480720ea924faf9645c1a1
jaeilwiki <34488287+jaeil-wiki@users.noreply.github.com>
2023-09-24 13:30:36 +0900

    upload missing source code & found caching issue

--- app/room/[roomId]/route.js
+++ app/room/[roomId]/route.js
@@ -1,196 +1,22 @@
-// mocks & constants
-const places = ["Lobby", "Cafeteria", "ConfRoom1", "ConfRoom2"];
-// Props
-const userAccountId = props.accountId || context.accountId;
-// States
-State.init({
-  place: props.place || 0,
-  users: [],
-  attendees: [],
-});
-// methods
-const getProfile = (accountId) => {
-  const p = Social.getr(`${accountId}/profile`);
-  const name = p?.name;
-  const image = p?.image;
-  const imageUrl =
-    image && image.ipfs_cid
-      ? `https://ipfs.near.social/ipfs/${image?.ipfs_cid}`
-      : "https://thewiki.io/static/media/sasha_anon.6ba19561.png";
+import {NextResponse} from "next/server";
+import {getMockAdvertisements, setMockAdvertisement, setMockAttendee} from "@/lib/storage";
 
-  return {
-    name,
-    imageUrl,
-  };
-};
-
-// Sub Components
-const Theme = styled.div`
-${
-  fetch(
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-  ).body
+export async function GET(req, res, {params}) {
+  const {roomId} = params
+  const users = await getMockAdvertisements(roomId || 0)
+  return NextResponse.json(users.map((k) => k.split(':').slice(-1)[0]))
 }
-`;
 
-const Banner = () => (
-  <div className="navbar bg-body-tertiary border rounded px-3 mb-3 justify-content-center">
-    <a className="navbar-brand" href="#">
-      <h2>nearbyme.social</h2>
-    </a>
-  </div>
-);
+export async function POST(req, {params}) {
+  const {roomId} = params
+  const {accountId} = await req.json()
+  if (!accountId || accountId === 'undefined' || accountId === 'null') {
+    return NextResponse.error(new Error('accountId is required'))
+  }
+  await setMockAttendee(accountId)
+  await setMockAdvertisement(roomId, accountId)
+  const users = await getMockAdvertisements(roomId)
+  console.log('POST/ users', users)
 
-const ProfileCard = ({ name, accountId, imageUrl }) => (
-  <a
-    href={`https://near.social/mob.near/widget/ProfilePage?accountId=${accountId}`}
-    style={{ textDecoration: "none" }}
-  >
-    <div className="navbar bg-body-tertiary border rounded px-3 mb-3">
-      <div className="row">
-        <div className="col-3 mt-4">
-          <img
-            className="profileImage"
-            src={imageUrl}
-            style={{ height: "40px" }}
-          />
-        </div>
-        <div className="col-9 pt-3">
-          <h4>{name}</h4>
-          <p>@{accountId.slice(0, 14)}...</p>
-        </div>
-      </div>
-    </div>
-  </a>
-);
-
-const BesideUsers = () => {
-  return (
-    <div className="row">
-      {!state.users ||
-        (state.users.filter((accountId) => accountId !== userAccountId)
-          .length === 0 && (
-          <div class="fs-5 my-5 w-100 text-center" style={{ color: "gray" }}>
-            {" "}
-            There is no users in place{" "}
-          </div>
-        ))}
-      {state.users &&
-        state.users
-          .filter((accountId) => accountId !== userAccountId)
-          .map((accountId) => {
-            const { name, imageUrl } = getProfile(accountId);
-            return (
-              <div className="col-6">
-                <ProfileCard
-                  name={name}
-                  accountId={accountId}
-                  imageUrl={imageUrl}
-                />
-              </div>
-            );
-          })}
-    </div>
-  );
-};
-
-const Attendees = () => {
-  return (
-    <div className="row">
-      {state.attendees &&
-        state.attendees.map((accountId) => {
-          const { name } = getProfile(accountId);
-          return (
-            <div className="col-4">
-              <a
-                href={`https://near.social/mob.near/widget/ProfilePage?accountId=${accountId}`}
-              >
-                <div className="navbar bg-body-tertiary border rounded px-3 mb-3 justify-content-center">
-                  <div className="text-truncate">{name}</div>
-                </div>
-              </a>
-            </div>
-          );
-        })}
-    </div>
-  );
-};
-
-const beaconAdvertise = () => {
-  asyncFetch(`https://collegium.runafter.build/room/${state.place}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    cache: "no-store",
-    body: JSON.stringify({
-      accountId: userAccountId,
-    }),
-  });
-};
-
-const BeaconSimulator = () => {
-  return (
-    <div
-      className="mb-3 px-1 border"
-      style={{ borderBottom: "solid 1px gray" }}
-    >
-      <div className="row">
-        <div className="fs-3">Beacon Simulator</div>
-        <div className="fs-5">You are in: {places[state.place]}</div>
-      </div>
-      <div className="row">
-        {places &&
-          places.map((location, idx) => (
-            <div key={idx} className="col-3 mb-2">
-              <a
-                href={`https://near.social/58423e7fe4bbc6956ea637d23cbeedee8ec23873fcd93bafb69086af625563e9/widget/NearbyMe?place=${idx}`}
-              >
-                <div className="btn btn-primary w-100">{location}</div>
-              </a>
-            </div>
-          ))}
-      </div>
-    </div>
-  );
-};
-
-const { name, imageUrl } = getProfile(userAccountId);
-const fetchScanned = async () => {
-  asyncFetch(`https://collegium.runafter.build/room/${state.place}`, {
-    cache: "no-store",
-  }).then((res) => {
-    State.update({ users: res.body });
-  });
-};
-
-const fetchAttendees = async () => {
-  asyncFetch("https://collegium.runafter.build/room", {
-    cache: "no-store",
-  }).then((res) => {
-    State.update({ attendees: res.body });
-  });
-};
-useEffect(() => {
-  fetchScanned();
-  fetchAttendees();
-  setInterval(() => {
-    fetchScanned();
-    beaconAdvertise();
-  }, 3000);
-}, []);
-
-return (
-  <Theme>
-    <div className="container-fluid mt-3 pb-3">
-      <BeaconSimulator />
-      <Banner />
-      <ProfileCard name={name} accountId={userAccountId} imageUrl={imageUrl} />
-      <p className="fs-3">Builders nearby me</p>
-      <BesideUsers />
-      <p className="fs-3">Collegium Contest Attendees</p>
-      <Attendees />
-    </div>
-  </Theme>
-);
+  return NextResponse.json(users.map((k) => k.split(':').slice(-1)[0]))
+}
--- app/room/route.js
+++ app/room/route.js
@@ -3,5 +3,6 @@ import {getMockAdvertisements, getMockAttendees} from "@/lib/storage";
 
 export async function GET() {
   const users = await getMockAttendees()
+  // FIXME : storage result is cached though it has no-cache header
   return NextResponse.json(users.map((k) => k.split(':').slice(-1)[0]))
 }
